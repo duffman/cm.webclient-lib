a53c600bc802bc580a61cb8567835e6ab4d35fa7
duffman <patrik.forsberg@coldmind.com>
2020-01-19 03:50:17 +0100

    Initial commit aften migration

--- src/tspath-require.js
+++
@@ -1,6 +0,0 @@
-var Module = require('module');
-var sysReq = Module.prototype.require;
-Module.prototype.require = function () {
-    //console.log("Require override");
-    return sysReq.apply(this, arguments);
-};
